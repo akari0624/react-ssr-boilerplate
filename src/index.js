3d3fdae25f9e3debb3fcc2d88880187d1b90aee8
akari0624 <akari0624@hotmail.com>
2018-01-04 19:04:00 +0800

    deploy react router root route

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -10,6 +11,13 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+   <BrowserRouter>
+     <div>
+        <Switch>
+           <Route path="/" component={App} />
+        </Switch>
+     </div> 
+   </BrowserRouter> 
   </Provider>
+
   , document.querySelector('.container'));
