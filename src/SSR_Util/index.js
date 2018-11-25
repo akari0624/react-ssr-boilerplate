a061d3f3e7ae8e0ce99cd8b194adad8bcde691f5
akari0624 <akari0624@hotmail.com>
2018-11-26 00:28:09 +0800

    fix one_tv_page ssr render, the ssr html is empty problem

--- src/SSR_Util/index.js
+++ src/SSR_Util/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import {StaticRouter, Route} from 'react-router-dom';
 
-export const generateTempRouteWhenSSR = (path, routedComponent, initProps) => {
+export const generateTempRouteWhenSSR = (path, RoutedComponent, initProps) => {
    
+    const context = {};
+
+
+/**
+ * Line 17 used to be, but the html return by react-dom/server.renderToString  will become empty string, WHY ??
+ * <Route path={path} render={() => <RoutedComponent data={initProps} /> } />
+ *  */
+
     if(initProps){
         return (
-            <StaticRouter>
-                <Route path={path} render={() => <routedComponent data={initProps} />} />
+            <StaticRouter  context={context}>
+                <RoutedComponent data={initProps} />
             </StaticRouter>  
         );
     }else{
         return (
-            <StaticRouter>
-                <Route path={path} component={routedComponent} />
+            <StaticRouter  context={context}>
+                <Route path={path} component={RoutedComponent} />
             </StaticRouter>  
         );
     }
 
 };
+
